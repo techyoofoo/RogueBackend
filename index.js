9fcc25a055541621fbeb0f56b155ea23ca712b94
surndraraj3 <surendraraj3@gmail.com>
2019-09-11 18:48:21 +0530

    Deleted cmments code

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ const bodyParser = require("body-parser");
 var fs = require("fs");
 const fsExtra = require("fs-extra");
 
-
 // console.log("Hello Node.js project.");
 // console.log(process.env.FRONT_END_APP_PATH);
 // dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
-const app_path =process.env.FRONT_END_APP_PATH;
+const app_path = process.env.FRONT_END_APP_PATH;
 // fs.appendFile('D:/ReactProject/Rogue-Single-SPA/src/plugins/IMAGE-1567684019063.txt', 'Hello content! \r\n', function (err) {
 //   if (err) throw err;
 //   console.log('Saved!');
 // });
-  console.log("Server Started");
+console.log("Server Started");
 app.use(cors());
 app.use(express.json());
 // app.use(bodyParser);
@@ -58,12 +57,11 @@ app.post("/upload", function(req, res) {
         unzipper.extract({ path: dir });
       }
 
-
-            if (fs.existsSync(chkPath)) {
+      if (fs.existsSync(chkPath)) {
         // Do something
-        console.log("not found");
+        console.log("Selected Packages", req.body.package);
 
-        const menuName = req.body.pluginName;
+        const menuName = req.body.name;
         const data = fs
           .readFileSync(`${app_path}index.html`)
           .toString()
@@ -95,9 +93,9 @@ app.post("/upload", function(req, res) {
           console.error(err);
         }
       }
-      
+
       setTimeout(function() {
-        const removeZipFIle = req.file.destination + "/" + req.file.filename;       
+        const removeZipFIle = req.file.destination + "/" + req.file.filename;
         fs.unlinkSync(removeZipFIle, function(err) {
           if (err) throw err;
           console.log("File Deleted");
@@ -110,78 +108,6 @@ app.post("/upload", function(req, res) {
     }
   });
 });
-// app.post("/upload", async function(req, res) {
-//   upload(req, res, async function(err) {
-//     console.log("Request ---", req.body.pluginName);
-//     console.log("Request file ---", req.file); //Here you get file.
-//     if (req.file) {
-//       var filepath = path.join(req.file.destination, req.file.filename);
-//       var unzipper = new Unzipper(filepath);
-
-//       unzipper.on("extract", function() {
-//         console.log("Finished extracting");
-//       });
-//       const splitextension = req.file.filename.split(".");
-//       const dir = req.file.destination + "/" + splitextension[0];
-//       // console.log("Directory", dir, "---", splitextension[0]);
-//       var fs = require("fs");
-//       if (!fs.existsSync(dir)) {
-//         fs.mkdirSync(dir);
-//         unzipper.extract({ path: dir });
-//       }
-
-//       //const testFolder1 = "D:/Rogue/RogueAppFrontend/webpack.config1.js";
-//       if (fs.existsSync(dir + "/index.js")) {
-//         // Do something
-//         console.log("not found");
-
-//         const menuName = req.body.pluginName;
-//         const data = fs
-//           .readFileSync(`${app_path}index.html`)
-//           .toString()
-//           .split("\n");
-//         data.splice(
-//           data.findIndex(x => x === "    </ul>"),
-//           0,
-//           `<a onclick="singleSpaNavigate('/` +
-//             menuName +
-//             `')"><li>` +
-//             menuName +
-//             `</li></a>`
-//         );
-//         const text = data.join("\n");
-//         fs.writeFile(`${app_path}index.html`, text, function(err) {
-//           if (err) return console.log(err);
-//         });
-//       } else {
-//         //const removeZipFIle ='D:/Rogue/RogueAppFrontend/src/plugins/'+req.body.pluginName;
-//         console.log("File Deleted", dir);
-//         try {
-//           await fsExtra.emptyDir(dir);
-//           console.log("success!");
-//           fs.rmdir(dir, function(err) {
-//             if (err) throw err;
-//             console.log("File Deleted");
-//           });
-//         } catch (err) {
-//           console.error(err);
-//         }
-//       }
-
-//       setTimeout(function() {
-//         const removeZipFIle = req.file.destination + "/" + req.file.filename;
-//         fs.unlinkSync(removeZipFIle, function(err) {
-//           if (err) throw err;
-//           console.log("File Deleted");
-//         });
-//       }, 10000);
-//     }
-//     /*Now do where ever you want to do*/
-//     if (!err) {
-//       return res.send(200).end();
-//     }
-//   });
-// });
 
 app.post("/test", (req, res) => {
   console.log("---", req.body);
